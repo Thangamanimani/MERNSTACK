7463dcab3fbb73ee2669f7296ae2e690e074f26a
Thangamanimani <manimklr.2000@gmail.com>
2025-09-07 09:08:28 +0530

    js code added

---
+++ javascript/day1.js
@@ -0,0 +1,68 @@
+//global scope
+//var a=5;
+//blocked scope
+//{
+//let b=10;
+//}
+//blocked and immutable
+//{
+//const c=20;
+//console.log(a)
+//}
+
+//array
+// var arr=[10,20,30,"hello",true,null]
+    
+//     arr.shift(100)
+//    console.log(arr)
+
+//modyfy and add
+    //var arr=[10,20,30,"hello",true,null]
+    //arr[2]=50;
+
+    //console.log(arr)
+
+    //remove element arrey
+    //pop is remove the elemennt of the end
+    //shift remove the element of the begining
+   // var arr=[10,20,30,"hello",true,null]
+    //arr.pop();
+    //console.log(arr)
+
+//    var arr=[10,20,30,"hello",true,null]
+//    arr.shift();
+  //  console.log(arr);
+
+  //object
+  //onject is the collection of properties
+  //each properties have key and value
+  //list of diffrent datatype
+  //more than one elements
+  //object always enclosed with the curly brackets
+
+// var obj={
+//     name:"revamp",
+//     class:"mern",
+//     batch:19,
+//     classmates:["a","b","c"],
+//     things:{mobile:"iphone",laptop:"hp"}
+
+// };
+//access the object of prperties
+// two notation use dot . ,bracket ("string")
+
+ //console.log(obj.name);
+// console.log(obj["name"]);
+
+
+//adding the properties
+//obj["batch"]=2025;
+//console.log(obj);
+
+//modyfy
+//obj.name="mani";
+//console.log(obj);
+
+//remove
+//delete obj.batch
+//console.log(obj);
